b70e7a901d8539f57191fee6f87e1aea03760612
unknown <mvndss24@gmail.com>
2023-06-04 19:01:50 +0530

    Adding first code

---
+++ Desktop/Code_java/Hello.java
@@ -0,0 +1,9 @@
+class First{
+    public static void main(String[] args) {
+        //int[][] arr=new int[2][5];
+        //int n =arr.length;
+        int[][] arr={{1,2,3,4,5},{2,3,4,5,6}};
+        arr[0][1]=2;
+        System.out.print(arr);        
+    }
+}
